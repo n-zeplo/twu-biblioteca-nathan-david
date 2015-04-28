f64a4a06d2530e35ffd473ceecb1be5f13418fe4
nzeplo <nzeplowitz@gmail.com>
2015-04-28 15:26:36 -0500

    Refactor All Tests with Before Method

--- test/com/twu/biblioteca/BibliotecaTest.java
+++ test/com/twu/biblioteca/BibliotecaTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.PrintStream;
@@ -14,11 +15,19 @@ import static org.mockito.Mockito.*;
  */
 public class BibliotecaTest {
 
+    private PrintStream printStream;
+    private Biblioteca biblioteca;
+
+    @Before
+    public void setUp() throws Exception {
+
+        printStream = mock(PrintStream.class);
+        biblioteca = new Biblioteca(printStream);
+    }
+
+
     @Test
     public void shouldWelcomeUserOnStartTest() {
-        PrintStream printStream = mock(PrintStream.class);
-
-        Biblioteca biblioteca = new Biblioteca(printStream);
         biblioteca.startUserInterface();
 
         verify(printStream).println(contains("Welcome"));
@@ -27,12 +36,10 @@ public class BibliotecaTest {
 
     @Test
     public void shouldPrintListOfBooksTest() {
-        PrintStream printStream = mock(PrintStream.class);
-
-        Biblioteca biblioteca = new Biblioteca(printStream);
         biblioteca.listBooks();
 
         verify(printStream).println(contains("1."));
         verify(printStream).println(contains("2."));
     }
+
 }
